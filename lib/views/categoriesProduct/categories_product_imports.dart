4a7350cc79116ea84e4f9b28f163cbd408257022
Abdelrahman0Atef <atefabdo08@gmail.com>
2025-05-14 14:41:40 +0300

    Working On Edit Reset API Class And Add Product Details View

--- lib/views/categoriesProduct/categories_product_imports.dart
+++ lib/views/categoriesProduct/categories_product_imports.dart
@@ -6,7 +6,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:untitled/core/services/locator_services/locator_services.dart';
 import 'package:untitled/core/services/rest_api_services/rest_api_services.dart';
 import 'package:untitled/views/home/home_imports.dart';
-import 'package:untitled/views/search/search_imports.dart';
 import 'package:untitled/widgets/products_grid_view.dart';
 import 'package:untitled/widgets/widgets_imports.dart';
 import '../../core/resources/my_strings.dart';
--- lib/views/product/product_imports.dart
+++ lib/views/product/product_imports.dart
@@ -4,7 +4,6 @@ import 'package:untitled/core/generic_cubit/generic_cubit.dart';
 import 'package:untitled/core/models/product_model/api_product_model.dart';
 import 'package:untitled/core/services/locator_services/locator_services.dart';
 import 'package:untitled/core/services/rest_api_services/rest_api_services.dart';
-import 'package:untitled/views/categoriesProduct/categories_product_imports.dart';
 import 'package:untitled/views/home/home_imports.dart';
 import 'package:untitled/widgets/products_grid_view.dart';
 import '../../core/resources/my_colors.dart';
